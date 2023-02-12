d730ef791df523eb3d266ac4dfd0d44896b84e00
youda <yudahyeon2@naver.com>
2023-02-12 16:27:51 +0900

    property 사용하여 캡슐화한 답안 코드

--- Four_Pillars_Of_OOP/Encasulation/CreditCard.py
+++ Four_Pillars_Of_OOP/Encasulation/CreditCard.py
@@ -1,44 +1,45 @@
 class CreditCard:
-    '''신용 카드를 나타내는 클래스'''
     MAX_PAYMENT_LIMIT = 30000000
 
     def __init__(self, name, password, payment_limit):
-        '''사용자 이름, 비밀번호 , 카드 한도 정보'''
-        self.__name = name
-        self.__password = password
-        self.__payment_limit = payment_limit
+        '''이름, 비밀번호 , 카드 한도'''
+        self.name = name
+        self.password = password
+        self.payment_limit = payment_limit
     
-    def get_name(self):
-        return self.__name
+    @property
+    def password(self):
+        '''password getter method'''
+        return "비밀 번호는 볼 수 없습니다"
     
-    def set_name(self,name):
-        self.__name = name
+    @password.setter
+    def password(self, passwd):
+        '''password setter method'''
+        self._password = passwd
     
-    def get_password(self):
-        print("비밀번호는 볼 수 없습니다.")
+    @property
+    def payment_limit(self):
+        '''payment_limit getter method'''
+        return self._payment_limit
     
-    def set_password(self, password):
-        self.__password = password
-    
-    def get_payment_limit(self):
-        return self.__payment_limit
-    
-    def set_payment_limit(self, payment_limit):
-        if payment_limit<0 or payment_limit>self.MAX_PAYMENT_LIMIT:
-            print("카드 한도는 0원~3천만원 사이로 설정해 주세요!")
-        else :self.__payment_limit = payment_limit
-
+    @payment_limit.setter
+    def payment_limit(self, limit):
+        '''payment_limit setter method'''
+        if limit<0 or limit>self.MAX_PAYMENT_LIMIT:
+             print("카드 한도는 0원 ~ 3천만 원 사이로 설정해 주세요!")
+        else: 
+            self._payment_limit = limit
 
 card = CreditCard("강영훈", "123", 100000)
 
-print(card.get_name())
-print(card.get_password())
-print(card.get_payment_limit())
+print(card.name)
+print(card.password)
+print(card.payment_limit)
 
-card.set_name("성태호")
-card.set_password("1234")
-card.set_payment_limit(-10)
+card.name = "성태호"
+card.password = "1234"
+card.payment_limit = -10
 
-print(card.get_name())
-print(card.get_password())
-print(card.get_payment_limit())
+print(card.name)
+print(card.password)
+print(card.payment_limit)
